dcedefbf9421b729b01c634f0dfa6397da34e5e7
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-10-27 12:50:07 +0800

    Update receipt_item_model.py

--- Src/Models/receipt_item_model.py
+++ Src/Models/receipt_item_model.py
@@ -1,19 +1,51 @@
 from Src.Core.abstract_model import abstact_model
 from Src.Models.nomenclature_model import nomenclature_model
 from Src.Models.range_model import range_model
+from Src.Core.validator import validator
 
 # Модель элемента рецепта
 class receipt_item_model(abstact_model):
-    __nomenclature:nomenclature_model
-    __range:range_model
-    __value:int
+    __nomenclature: nomenclature_model
+    __range: range_model
+    __value: int
+
+    # Номенклатура
+    @property
+    def nomenclature(self) -> nomenclature_model:
+        return self.__nomenclature
+
+    @nomenclature.setter
+    def nomenclature(self, value: nomenclature_model):
+        validator.validate(value, nomenclature_model)
+        self.__nomenclature = value
+
+    # Единица измерения
+    @property
+    def range(self) -> range_model:
+        return self.__range
+
+    @range.setter
+    def range(self, value: range_model):
+        validator.validate(value, range_model)
+        self.__range = value
+
+    # Значение
+    @property
+    def value(self) -> int:
+        return self.__value
+
+    @value.setter
+    def value(self, value: int):
+        validator.validate(value, int)
+        if value <= 0:
+            raise ValueError("Значение должно быть больше 0")
+        self.__value = value
 
     # Фабричный метод
-    def create(nomenclature:nomenclature_model, range:range_model,  value:int):
+    @staticmethod
+    def create(nomenclature: nomenclature_model, range: range_model, value: int):
         item = receipt_item_model()
-        item.__nomenclature = nomenclature
-        item.__range = range
-        item.__value = value
-
-
-    # TODO: Внимание! Тут нужно добавить свойства
+        item.nomenclature = nomenclature
+        item.range = range
+        item.value = value
+        return item
