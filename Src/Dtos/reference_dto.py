be8c561d5f916a9663dbc11bb7e2ce7cdf0efff0
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-12-02 09:37:59 +0800

    Update reference_dto.py

--- Src/Dtos/reference_dto.py
+++ Src/Dtos/reference_dto.py
@@ -1,13 +1,20 @@
+"""
+DTO для операций со справочниками
+Содержит информацию о типе модели и её свойствах
+"""
 from Src.Core.abstract_dto import abstact_dto
 from Src.Core.validator import validator
 
 class reference_dto(abstact_dto):
     __model_dto_dict: dict = None
-    
+
+    """
+    Словарь свойств модели в формате DTO
+    """
     @property
     def model_dto_dict(self) -> dict:
         return self.__model_dto_dict
-    
+
     @model_dto_dict.setter
     def model_dto_dict(self, value):
         validator.validate(value, dict)
