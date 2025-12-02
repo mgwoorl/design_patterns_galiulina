2fe9b9a5bdc8007fd6752028863aac702d92574d
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2025-12-02 09:38:33 +0800

    Update check_dependencies_dto.py

--- Src/Dtos/check_dependencies_dto.py
+++ Src/Dtos/check_dependencies_dto.py
@@ -1,14 +1,20 @@
+"""
+DTO для проверки зависимостей перед удалением объекта
+"""
 from Src.Core.abstract_dto import abstact_dto
 from Src.Models.abstract_reference import abstact_reference
 from Src.Core.validator import validator
 
 class check_dependencies_dto(abstact_dto):
     __model: abstact_reference = None
-    
+
+    """
+    Модель для проверки зависимостей
+    """
     @property
     def model(self) -> abstact_reference:
         return self.__model
-    
+
     @model.setter
     def model(self, value):
         validator.validate(value, abstact_reference)
